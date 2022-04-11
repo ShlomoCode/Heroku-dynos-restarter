08ce2dd4a4d21a25167a319c4d8ba6b6c935e3d3
ShlomoCode <shlomomail16@gmail.com>
2022-04-11 17:20:20 +0300

    Create app

---
+++ README.md
@@ -0,0 +1,37 @@
+# Heroku dynos restarter
+
+אפליקציה לאתחול הדינו'ס של אפליקציית נוד כבדה על על הרקו, במקרה שהיא קרסה. נוצר עבור https://madrichim.ovh.
+
+# Config example
+
+In `config.env` file in root directory.
+```
+TOKEN_API_HEROKU=fc0d678d-6b63-4b93-b7bc-19e5fbe8e3r9 #טוקן heroku
+APP_NAME=madrichim #שם האפליקציה בהרקו
+SITE_URL=https://madrichim.ovh #הכתובת שאותה יש לנטר
+```
+
+# Deploy in heroku
+יש ללחוץ על הכפתור 👇👇 ולמלא את הקונפיג.
+<div  align='right'>
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+</div>
+
+## מניעת "הירדמות" אוטומטית
+בתוכנית החינמית של heroku, [השרת נכבה מעצמו](https://devcenter.heroku.com/articles/free-dyno-hours#dyno-sleeping) אחרי 30 דקות ללא בקשה חיצונית לשרת.
+ניתן לעקוף זאת על ידי [אימות אשראי בחשבון ההרקו](https://devcenter.heroku.com/articles/account-verification), וכך מקבלים סה"כ 1000 שעות חינם בחודש, שמספיקות לפעילות רציפה של האפליקציה.
+לאחר מכן יש לשלוח "בקשת דמה" לאפליקציה כל פחות מ-30 דקות.
+ ניתן לעשות זאת בקלות באמצעות אחד האתרים הבאים:
+* https://kaffeine.herokuapp.com
+* https://www.downnotifier.com
+
+## מראי מקומות
+https://devcenter.heroku.com/articles/platform-api-reference#dyno-restart-all
+
+### יצירת טוקן קבוע:
+```
+$ heroku authorizations:create
+```
+https://devcenter.heroku.com/articles/platform-api-quickstart#authentication
---
+++ app.js
@@ -0,0 +1,51 @@
+const axios = require('axios');
+const ms = require('ms');
+require('dotenv').config({ path: 'config.env' });
+const express = require('express');
+const app = express();
+
+axios.defaults.headers.delete.Authorization = `Bearer ${process.env.TOKEN_API_HEROKU}`;
+axios.defaults.headers.delete['Content-Type'] = 'application/json';
+axios.defaults.headers.delete.Accept = 'application/vnd.heroku+json; version=3';
+
+function sleep (ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+};
+
+(async () => {
+    console.log('🚀start...');
+    // infinite loop
+    do {
+        const appName = process.env.APP_NAME;
+        if (!appName) return console.log('APP_NAME is required!');
+        try {
+            await axios.get(process.env.SITE_URL || `https://${appName}.herokuapp.com/`);
+            console.log('😎application OK!');
+        } catch (error) {
+            if (error.response.status !== 200) {
+                console.log(`🥵application error!! (status ${error.response.status}) 🛠️Restarting dynos, please wait...`);
+                try {
+                    const resetDynosResponse = await axios.delete(`https://api.heroku.com/apps/${appName}/dynos`);
+                    console.log(`Successful operation: ${resetDynosResponse.status === 202}`);
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        }
+        const WaitingTime = '50s';
+        console.log(`⏳Waiting ${WaitingTime}. please wait...`);
+        await sleep(ms(WaitingTime));
+        console.log('✅Waiting completed!');
+    } while (true);
+})();
+
+app.use('/', (req, res) => {
+    res.status(200).json({ message: 'OK' });
+});
+
+const http = require('http');
+const port = process.env.PORT || 3500;
+const server = http.createServer(app);
+server.listen(port);
